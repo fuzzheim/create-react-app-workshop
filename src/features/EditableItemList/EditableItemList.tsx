c406c50c406db2000cd9ddf6585644bae18744a3
Thomas Haugland Rudfoss <thomas.rudfoss@gmail.com>
2020-02-06 10:16:20 +0100

    Finalized EditableItemList

--- src/features/EditableItemList/EditableItemList.tsx
+++ src/features/EditableItemList/EditableItemList.tsx
@@ -1,12 +1,38 @@
 import React, { useState } from "react"
 
+interface IEditableItem {
+	id: number
+	text: string
+}
+
+let currentId = 0
+const genId = () => {
+	return currentId++
+}
+
 export const EditableItemList: React.FC = () => {
-	const [ items, setItems ] = useState([])
+	const [ items, setItems ] = useState<IEditableItem[]>([])
 	const [ newItemText, setNewItemText ] = useState("")
+
+	const inputChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setNewItemText(event.target.value)
+	}
+	const inputKeyUpHandler = (event: React.KeyboardEvent<HTMLInputElement>) => {
+		if (event.key === "Enter") {
+			const newItems = items.slice()
+			newItems.push({
+				id: genId(),
+				text: newItemText
+			})
+			setItems(newItems)
+			setNewItemText("")
+		}
+	}
 
 	return (
 		<div>
-			<input type="text" value={newItemText} />
+			<input type="text" value={newItemText} onChange={inputChangeHandler} onKeyUp={inputKeyUpHandler} />
+			<ul>{items.map((item) => <li key={item.id}>{item.text}</li>)}</ul>
 		</div>
 	)
 }
