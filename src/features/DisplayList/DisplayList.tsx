de40df5a684e191303fe3286958ca0e157146be1
Thomas Haugland Rudfoss <thomas.rudfoss@gmail.com>
2020-02-06 13:11:42 +0100

    Added remote user list

--- src/features/DisplayList/DisplayList.tsx
+++ src/features/DisplayList/DisplayList.tsx
@@ -1,13 +1,27 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import DisplayListTable from "./DisplayListTable"
-import { DISPLAY_LIST_DATA } from "./DISPLAY_LIST_DATA"
 import DisplayListSearch from "./DisplayListSearch"
+import { IDisplayListUser } from "./IDisplayListUser"
+
+import { fetchUsers } from "api/fetchUsers"
+
+const getUserList = async (setter: (users: IDisplayListUser[]) => any, clearLoading: () => any) => {
+	const users = await fetchUsers()
+	setter(users)
+	clearLoading()
+}
 
 export const DisplayList: React.FC = () => {
 	const [ searchText, setSearchText ] = useState("")
+	const [ users, setUsers ] = useState<IDisplayListUser[]>([])
+	const [ isLoading, setIsLoading ] = useState(true)
+
+	useEffect(() => {
+		getUserList(setUsers, () => setIsLoading(false))
+	}, [])
 
 	const normalizedSearchText = searchText.trim().toLocaleLowerCase()
-	const filteredList = DISPLAY_LIST_DATA.filter((user) => {
+	const filteredList = users.filter((user) => {
 		if (normalizedSearchText === "") return true
 		if (user.name.toLocaleLowerCase().indexOf(normalizedSearchText) >= 0) return true
 		if (user.username.toLocaleLowerCase().indexOf(normalizedSearchText) >= 0) return true
@@ -17,8 +31,17 @@ export const DisplayList: React.FC = () => {
 
 	return (
 		<React.Fragment>
-			<DisplayListSearch searchText={searchText} onSearchTextChange={setSearchText} />
-			<DisplayListTable users={filteredList} />
+			{isLoading && (
+				<p>
+					<strong>Loading data...</strong>
+				</p>
+			)}
+			{!isLoading && (
+				<React.Fragment>
+					<DisplayListSearch searchText={searchText} onSearchTextChange={setSearchText} />
+					<DisplayListTable users={filteredList} />
+				</React.Fragment>
+			)}
 		</React.Fragment>
 	)
 }
